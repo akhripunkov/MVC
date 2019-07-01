bc72e59b7b1b687f05a5f98a13deca05368421a8
akhripunkov <Alexander-e-mail@yandex.ru>
2019-07-01 14:42:46 +0300

    Add files via upload
    MVC Project

---
+++ WindowsFormsApp1/WindowsFormsApp1/Controllers/GetInfoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Controllers
+{
+    class GetInfoController
+    {
+        public string Questinon(string msg)
+        {
+            Model model = new Model();
+            return model.GetAnswer(msg);
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Database.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    static class Database
+    {
+        public static string GetAnswer(string question) {
+
+            string path = @"Tables/Books.txt"; //путь к БД
+            using (StreamReader sr = new StreamReader(path, Encoding.Default))
+            {
+                string str = null;
+
+                while (true)
+                {
+                    str = sr.ReadLine();
+                    if (str != null)
+                    {
+                        if (str == question)
+                        {
+                            return question + " есть в наличии";
+                        }
+                    }
+                    else
+                        return question + " нет в наличии";
+                }
+
+            }
+            
+        }
+    }
+}
